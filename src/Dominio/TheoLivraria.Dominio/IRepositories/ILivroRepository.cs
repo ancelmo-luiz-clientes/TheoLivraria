134632a0ae4072eb21a8758d50b5b255915f0025
Ancelmo Luiz <ancelmosantos@setic.ro.gov.br>
2021-05-11 23:49:46 -0400

    Adicionar arquivos de projeto.

---
+++ src/Dominio/TheoLivraria.Dominio/IRepositories/ILivroRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheoLivraria.Dominio.Entidades;
+
+namespace TheoLivraria.Dominio.IRepositories
+{
+    public interface ILivroRepository
+    {
+        Task Criar(Livro livro);
+
+        Task Atualizar(Livro livro);
+
+        Task Excluir(Livro livro);
+
+        Task<Livro> BuscarPorId(int id);
+
+        Task<IEnumerable<Livro>> ListarTodos();
+    }
+}
